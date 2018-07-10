627cb4c5ca98a590e08e539900adf5462f25e46c
Chad McDermott <chadmcdermott1997@gmail.com>
2018-07-10 13:15:02 -0400

    mkgains.py: Added options for specifying the type of distribution as well as properties of the distribution

--- mkgains.py
+++ mkgains.py
@@ -11,11 +11,25 @@ import subprocess as sub
 # Start parsing arguments
 parse = argp.ArgumentParser()
 parse.add_argument("path", help='Path to data file', type=str)
-parse.add_argument('-i', '--integrations',  help='Number of integrations used for boxcar smoothing', type=int)
+parse.add_argument('-n', '--integrations',  help='Number of integrations used for boxcar smoothing', type=int)
 parse.add_argument('-f', '--frequency', help='Minimum frequency to start zeroing out (in Hz)', type=np.float64)
 parse.add_argument('-t', '--time', help='Amount of time (in seconds) used for boxcar smoothing', type=int)
+parse.add_argument('-a', '--amplitudephase', help='Use the Amplitude-Phase form for calculating fake gains',
+        action='store_true')
+parse.add_argument('-d', '--distribution', help='Select the type of random distribution used for finding the' + \
+        ' real and imaginary parts (or the amplitude and phase if -a is used) for the creation of the gains',
+        nargs=2, default=['normal', 'normal'], choices=['normal', 'uniform'])
+parse.add_argument('-r', '--realrange', help='If uniform distribution is selected for the real part' + \
+        ' (or the amplitude if -a is used), then this selects the minimum and maximum values for the' + \
+        ' distribution, in that order. Otherwise, it selects the mean and standard deviation of the' + \
+        ' normal distribution, in that order.', nargs=2, type=np.float64, default=[None, None])
+parse.add_argument('-i', '--imagrange', help='If uniform distribution is selected for the imaginary part' + \
+        ' (or the phase if -a is used), then this selects the minimum and maximum values for the' + \
+        ' distribution, in that order. Otherwise, it selects the mean and standard deviation of the' + \
+        ' normal distribution, in that order.', nargs=2, type=np.float64, default=[None, None])
 args = parse.parse_args()
 
+# Check and populate args
 fullpath = os.path.abspath(os.path.expanduser(args.path))
 if fullpath.endswith('/'):
     fullpath = fullpath[:-1]
@@ -25,6 +39,21 @@ if bool(args.time) == bool(args.integrations):
 
 if args.frequency == None:
     raise ValueError('-f must be used')
+
+rand_input = []
+for count, check in enumerate([args.realrange, args.imagrange]):
+    if check == [None, None]:
+        if args.distribution[0] == 'normal':
+            rand_input += [count ^ 1, .01]
+        elif args.distribution[0] == 'uniform':
+            if count == 0:
+                rand_input += [1, 10]
+            elif count == 1:
+                rand_input += [-np.pi, np.pi]
+    elif args.distribution[count] == 'uniform':
+        if check[0] >= check[1]:
+            raise ValueError('The first value must be smaller than the second' + \
+                    'value if uniform distribution was chosen for that input')
 
 # Load data
 uvd = UVData()
@@ -84,13 +113,28 @@ if uvc.x_orientation == None:
 # Create random gains
 gain_shape = (uvc.Nants_data, uvc.Nspws, uvc.Nfreqs, uvc.Ntimes, uvc.Njones)
 
-maxrand = np.random.randint(2, 150)
-minrand = np.random.randint(0, maxrand - 1)
-real = (((maxrand - minrand) * np.random.random(gain_shape)) + minrand)#.astype('complex128')
-imag = (((maxrand - minrand) * np.random.random(gain_shape)) + minrand) * 1j
-gains = real + imag
-print('min value: %d' % (minrand))
-print('max value: %d' % (maxrand))
+if args.amplitudephase:
+    names = ['Amplitude', 'Phase']
+else:
+    names = ['Real', 'Imaginary']
+
+distfuncts = []
+for count, dist in enumerate(args.distribution):
+    if dist == 'normal':
+        distfuncts += [np.random.normal]
+        descriptions = ['Mean', 'Standard Deviation']
+    elif dist == 'uniform':
+        distfuncts += [np.random.uniform]
+        descriptions = ['Minimum', 'Maximum']
+    print('%s %s: %f' % (names[count], descriptions[0], rand_input[count * 2]))
+    print('%s %s: %f' % (names[count], descriptions[1], rand_input[count * 2 + 1]))
+
+if args.amplitudephase:
+    gains = distfuncts[0](rand_input[0], rand_input[1], gain_shape) * \
+            np.exp(distfuncts[1](rand_input[2], rand_input[3], gain_shape) * 1j)
+else:
+    gains = distfuncts[0](rand_input[0], rand_input[1], gain_shape) + \
+            distfuncts[1](rand_input[2], rand_input[3], gain_shape) * 1j
 
 # Preparing variables for transformations
 if args.time:
